5ae63f0f4867204532d38b639023a01e3cdd02cd
webmaster@mediakod.com <webmaster@mediakod.com>
2020-03-21 12:00:03 +0100

    adding components homepage

---
+++ scripts/nav-toggle.js
@@ -0,0 +1,11 @@
+const toggle = document.querySelector('[aria-pressed]');
+
+toggle.addEventListener('click', (e) => {  
+  let pressed = e.target.getAttribute('aria-pressed') === 'true';
+  e.target.setAttribute('aria-pressed', String(!pressed));
+  /***toggle***/
+  e.preventDefault();
+  [].map.call(document.querySelectorAll('.navbar'), function(el) {
+    el.classList.toggle('navbar--active');
+  });
+});
